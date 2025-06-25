8c21ab52476dc52cfcbc99f8f2b4b78ee3aab88e
HarrisHeyer <hh@mtxcap.com>
2025-06-24 21:01:48 -0400

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>🏡 Super App MVP</h1>
-      <p>Welcome! Use the navigation above to explore your home management features.</p>
-    </main>
-  );
-}
